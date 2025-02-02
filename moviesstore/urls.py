d3278e546bfba5ef6e513e1d276e9c1c3789b2c5
tanishagupta24 <tanisha.v.gupta@gmail.com>
2025-02-02 18:16:35 -0500

    chapter 7 and chapter 8

--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
