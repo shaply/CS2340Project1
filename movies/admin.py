f065f35873b765fec4b6d272733bfaaf0f8017e0
David Gu <dawsomegu@gmail.com>
2025-02-02 17:41:22 -0500

    Display movies from database

--- movies/admin.py
+++ movies/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Movie
-admin.site.register(Movie)
+
 # Register your models here.
+
+admin.site.register(Movie)
--- movies/views.py
+++ movies/views.py
@@ -6,25 +6,13 @@ from .models import Review, Movie
 
 # Create your views here.
 from django.shortcuts import render
-movies = [
-    {
-        'id': 1, 'name': 'Inception', 'price': 12,
-        'description': 'A mind-bending heist thriller.'
-    },
-    {
-        'id': 2, 'name': 'Avatar', 'price': 13,
-        'description': 'A journey to a distant world and the battle for resources.'
-    },
-    {
-        'id': 3, 'name': 'The Dark Knight', 'price': 14,
-        'description': 'Gothams vigilante faces the Joker.'
-    },
-    {
-        'id': 4, 'name': 'Titanic', 'price': 11,
-        'description': 'A love story set against the backdrop of the sinking Titanic.',
-    },
-]
+
 def index(request):
+    search_term = request.GET.get('search', '')
+    if search_term:
+        movies = Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
     template_data = {}
     template_data['title'] = 'Movies'
     template_data['movies'] = movies
@@ -32,9 +20,9 @@ def index(request):
                   {'template_data': template_data})
 
 def show(request, id):
-    movie = movies[id - 1]
+    movie = Movie.objects.get(id=id)
     template_data = {}
-    template_data['title'] = movie['name']
+    template_data['title'] = movie.name
     template_data['movie'] = movie
     return render(request, 'movies/show.html',
                   {'template_data': template_data})
