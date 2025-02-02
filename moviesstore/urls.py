1bbf6933248c26b16af7b631249323aa232f8cf1
sarveshtiku <64361696+sarveshtiku@users.noreply.github.com>
2025-02-02 16:39:54 -0500

    Moveis

--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('movies/', include('movies.urls')),
 ]
