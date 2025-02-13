cd1b5a4570d2a250816fdb966d70cc6ce974af33
sarveshtiku <64361696+sarveshtiku@users.noreply.github.com>
2025-02-13 17:37:04 -0500

    Orders and Items Chapter 11

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order, Item
+admin.site.register(Item)
+
+admin.site.register(Order)
+from django.contrib import admin
 
 # Register your models here.
